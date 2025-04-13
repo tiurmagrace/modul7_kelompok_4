d988872dd5325de01340e966afd69477fb16ea6c
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-04-13 14:47:32 +0700

    menambahkan class DataMahasiswa<NIM>

---
+++ modul7_kelompok_4/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Deserialisasi Data Mahasiswa ===");
+        DataMahasiswa2311104073.ReadJSON();
+    }
+}
