5808eb02624154a0d96e41ff1c18e70e58d8c147
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-04-14 18:03:42 +0700

    menambahkan class DataMahasiswa2311104073

---
+++ modul7_kelompok_4/DataMahasiswa2311104073.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class DataMahasiswa2311104073
+{
+    public static void ReadJSON()
+    {
+        string fileName = "jurnal7_1_2311104073.json";
+
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("File tidak ditemukan.");
+            return;
+        }
+
+        string jsonString = File.ReadAllText(fileName);
+        var data = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+
+        Console.WriteLine("Data Mahasiswa:");
+        Console.WriteLine($"Nama  : {data.Nama}");
+        Console.WriteLine($"NIM   : {data.NIM}");
+        Console.WriteLine($"Umur  : {data.Umur}");
+        Console.WriteLine($"Kelas : {data.Kelas}");
+        Console.WriteLine($"Asal  : {data.Asal}");
+    }
+}
+
+public class Mahasiswa
+{
+    public string Nama { get; set; }
+    public string NIM { get; set; }
+    public int Umur { get; set; }
+    public string Kelas { get; set; }
+    public string Asal { get; set; }
+}
