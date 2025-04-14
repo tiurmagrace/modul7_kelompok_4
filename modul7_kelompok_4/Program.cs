8a40686c39295fefd6cd1debd783ddc814568dbb
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-04-14 12:13:56 +0700

    menambahkan class GlossaryItem2311104073

---
+++ modul7_kelompok_4/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("\n=== Deserialisasi Glossary Item ===");
+        GlossaryItem2311104073.ReadJSON();
+    }
+}
