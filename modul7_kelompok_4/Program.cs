1976375435be325de152c08c8a85e5d6f218921c
USERNAME_GITHUB_YANG_BENAR <EMAIL_GITHUB_YANG_BENAR>
2025-04-14 12:02:50 +0700

    menambahkan class TeamMembers2311104073

---
+++ modul7_kelompok_4/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\n=== Deserialisasi Team Members ===");
+        TeamMembers2311104073.ReadJSON();
+    }
+}
---
+++ modul7_kelompok_4/TeamMembers2311104073.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+
+public class TeamMembers2311104073
+{
+    public static void ReadJSON()
+    {
+        string fileName = "jurnal7_2_2311104073.json";
+
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("File tidak ditemukan.");
+            return;
+        }
+
+        string jsonString = File.ReadAllText(fileName);
+        var data = JsonSerializer.Deserialize<List<TeamMember>>(jsonString);
+
+        Console.WriteLine("Team member list:");
+        foreach (var member in data)
+        {
+            Console.WriteLine($"{member.NIM} {member.FirstName} {member.LastName} ({member.Age} {member.Gender})");
+        }
+    }
+}
+
+public class TeamMember
+{
+    public string NIM { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+}
