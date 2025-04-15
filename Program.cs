9cf2d8e1923b003322c78d278fa6343a2a470a06
Meph1sto14 <marvelsanjaya7@gmail.com>
2025-04-15 14:35:18 +0700

    menambahkan class DataMahasiswa2311104053

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa2311104053.ReadJSON();
+    }
+}
--- modul7_kelompok_4/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿class Program
-{
-    static void Main(string[] args)
-    {
-        DataMahasiswa2311104042.ReadJSON();
-        TeamMembers2311104042.ReadJSON();
-        GlossaryItem2311104042.ReadJSON();
-    }
-}
