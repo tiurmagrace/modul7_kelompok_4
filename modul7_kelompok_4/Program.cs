2cc91a7f94cbd3c3677f3159544513455642da64
Meph1sto14 <marvelsanjaya7@gmail.com>
2025-04-15 15:20:34 +0700

    merge ulang

--- modul7_kelompok_4/Program.cs
+++ modul7_kelompok_4/Program.cs
@@ -2,8 +2,5 @@
 {
     static void Main(string[] args)
     {
-        DataMahasiswa2311104042.ReadJSON();
-        TeamMembers2311104042.ReadJSON();
-        GlossaryItem2311104042.ReadJSON();
     }
 }
